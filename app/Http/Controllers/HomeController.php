a320c79411754dc2bd541363dcdeb613b4b6cad9
shamim <razed3322@gmail.com>
2024-03-14 01:00:48 +0600

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    public function index(){
+
+        if(Auth::id()){
+
+            $userType = Auth()->user()->userType;
+            if($userType == 'user'){
+                return view('dashboard');
+            } elseif($userType=='admin'){
+                return view('admin.adminHome');
+
+            }
+        }
+        else{
+             return redirect()->back();
+        }
+    }
+}
